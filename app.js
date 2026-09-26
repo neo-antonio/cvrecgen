061dc30260a55a02a74cc8febfe4bc3e65a4463d
Neo Antonio <neoantonio18@gmail.com>
2026-09-26 11:21:41 +0800

    fix ID

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const CONFIG = {
   siteLink: 'Court Vision',   // printed at the bottom of every receipt
   logo: 'icons/logo.png',         // printed at the top of every receipt
   portfolio: {
-    endpoint: 'https://script.google.com/macros/s/AKfycbzL7Fs7HbRE2LHWYqyO1edz5JZUQPI0RKAFI0LHf-BZ5xM9aKtvI738Soqh1FYsHVVw6g/exec',
+    endpoint: 'https://script.google.com/macros/s/AKfycbwC0wwCnfHy2qt3S-TOssgNLHtldosf77wKxcFWzR0VRQ1PFGVRYp5rbj2Ih0USc5o2SA/exec',
     secret: 'courtvision_$0819'   // must match the SECRET constant in Code.gs
   }
 };
